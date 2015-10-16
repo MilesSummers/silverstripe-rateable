1843e0d1a02ee3088413e95be689a64bb0d42148
MilesSummers <miles@youngearth.com.au>
2015-10-16 12:52:14 +1100

    Add inline messages rather than alerts

--- javascript/rateable.js
+++ javascript/rateable.js
@@ -5,24 +5,48 @@
 
 		rateables.each(function(){
 			var self = $(this);
+			var numratings = self.data('votes');
+			
 			self.raty({
 				readOnly 	: self.hasClass('disabled'),
 				score 		: self.data('averagescore'),
 				path 		: 'rateable/images/'
 			});
 
+			function NumRatings() {
+				if(numratings == 1) return numratings + ' vote';
+				else return numratings + ' votes';
+			}
+				
+			function ShowMessage(message) {
+				var oldhtml = NumRatings();
+				
+				$('#raty-message').fadeOut(500, function() {
+				    $('#raty-message').html(message).fadeIn();
+				    $('#raty-message').delay(2000);
+				    $('#raty-message').fadeIn(500, function() { $('#raty-message').html(oldhtml);  });
+				});	
+			}    
+			
+			function ShowRating() {
+				$('#raty-message').html(NumRatings());
+			}
+			
+			$('#raty-message').html(NumRatings());
+						
+
 			self.find('img').not('.raty-cancel').click(function(){
 				var img = $(this);
 				var score = img.attr('alt');
 
 				if(self.hasClass('disabled')){
-					alert("You have already rated this item");
+					ShowMessage("You have already rated this item");
 					return;	
 				} 
 
 				$.getJSON(self.data('ratepath') + '?score=' + score, function(data) {
 					if(data.status == 'error'){
-						alert(data.message);
+						ShowMessage(data.message);
 						return;
 					}
 
@@ -32,7 +56,7 @@
 					});
 
 					self.addClass('disabled');
-					alert(data.message);
+					ShowMessage(data.message);
 				});
 			});
 		});
--- templates/Includes/RateableUI.ss
+++ templates/Includes/RateableUI.ss
@@ -1,1 +1,1 @@
-<div id="$RatingHTMLID" class="$RatingCSSClass" data-averagescore="$AverageScore" data-ratepath="$RatePath"></div>
+<div id="$RatingHTMLID" class="$RatingCSSClass" data-averagescore="$AverageScore" data-ratepath="$RatePath" data-votes="$NumberOfRatings"></div><div id="raty-message"></div>
